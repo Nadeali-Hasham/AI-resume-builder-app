3f2077caa7c2dc07ce0543fe9249f288c5afd235
Nadeali-Hasham <syednadealihashamshah@gmail.com>
2026-07-03 16:27:45 +0500

    setup the project

--- Ai-resume-builder/src/App.jsx
+++ Ai-resume-builder/src/App.jsx
@@ -1,12 +1,14 @@
 // import { useState } from 'react'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
-      <h1>AI Resume Builder</h1>
-      <p>Build your resume with the power of AI</p>
+      <h1 className="text-4xl font-bold">AI Resume Builder</h1>
+      <p className="mt-2 text-lg">Build your resume with the power of AI</p>
+      <Button className>Get Started</Button>
     </>
   )
 }
